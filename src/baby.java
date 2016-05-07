670f41cc4c0b50e7e834f4aec2696d062e974c1d
Revanty <league@WTS14.attlocal.net>
2016-05-07 10:03:11 -0700

    day 7

---
+++ src/baby.java
@@ -0,0 +1,25 @@
+
+public class baby {
+	public static void main(String[] args) {
+		String name = "baby";
+		for (int a = 0; a < 2; a++) {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 3; j++) {
+					System.out.println("baby");
+				}
+				if (i == 0 || i == 2)
+
+				{
+					System.out.println("oh");
+
+				} else {
+					System.out.println("no");
+				}
+
+			}
+
+		}
+		System.out.println("I thought you'd alawys be mine");
+	}
+
+}
