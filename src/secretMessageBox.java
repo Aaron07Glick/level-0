08c3e0e56f55b67fb3fc13892508e3bfc5ff1f7f
techied <league@WTS5.attlocal.net>
2016-08-27 09:58:44 -0700

    lemon day

--- src/secretMessageBox.java
+++ src/secretMessageBox.java
@@ -3,10 +3,12 @@ import javax.swing.JOptionPane;
 public class secretMessageBox {
 	public static void main(String[] args) {
 		String passWord = "sixSixSix";
+		String userName = "Satan";
+		String input2 = JOptionPane.showInputDialog("Please enter a username");
 		String input = JOptionPane.showInputDialog("What is the passcode?");
 		// int userInput = Integer.parseInt(input);
-		if (input.equalsIgnoreCase("sixSixSix")) {
-			JOptionPane.showMessageDialog(null, "Satan has risen");
+		if (input2.equalsIgnoreCase("Satan") && input.equalsIgnoreCase("sixSixSix")) {
+			JOptionPane.showMessageDialog(null, "I AM SATAN LORD OF DARKNESS");
 		} else {
 			JOptionPane.showMessageDialog(null, "GO AWAY!");
 		}
